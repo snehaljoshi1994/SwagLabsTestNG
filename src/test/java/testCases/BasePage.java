c0445a1889b824fb3c5a04e8f3583db58594fd8a
Snehal Joshi <snehaljoshi0010@gmail.com>
2024-04-06 22:27:40 +0530

    added logic for selenium grid

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,20 +18,36 @@ public class BasePage {
 	@BeforeMethod
 	public void browserLaunch() throws MalformedURLException {
 
+		String DriverType = System.getProperty("Browser");
+		if (DriverType.contains("firefox")) {
+
+			driver = new FirefoxDriver();
+
+		} else if (DriverType.contains("remote")) {
+
 			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--no-sandbox"); 
-			options.addArguments("--disable-dev-shm-using") ;
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-using");
 			options.addArguments("--window-size=1920,1080");
 			options.addArguments("--headless");
-			options.addArguments("--ignore-ssl-errors=yes");
-			options.addArguments("--ignore-certificate-errors");
+			;
+			driver = new RemoteWebDriver(new URL("http://54.221.35.130:4444"), options);
+		}
+
+		else {
+
+			// added some comments
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-using");
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--headless");
 			driver = new ChromeDriver(options);
+		}
 
 		driver.get("https://www.saucedemo.com/v1/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		
-		
 
 	}
 
